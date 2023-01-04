4ef88904412eaec5083417158bd8f3a5c50d2b16
mak2220 <113451174+mak2220@users.noreply.github.com>
2023-01-04 17:38:02 -0300

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,11 @@
+let factorial = 1;
+let i = 10
+
+while(true){
+    factorial *=i;
+    i--
+    if(i<1){
+        break
+    }    
+}
+console.log(factorial)
---
+++ factorial-while.js
@@ -0,0 +1,8 @@
+let factorial = 1;
+let i = 10
+
+while(i > 0){
+    factorial *=i;
+    i--    
+}
+console.log(factorial)
